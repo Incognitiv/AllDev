1cb0e8312b3991c0f0b01c20daa6e9c2fefa1b55
Shiny <the.shiny.at@gmail.com>
2014-12-31 18:38:11 +0100

    Updated equalizer.js

--- js/equalizer.js
+++ js/equalizer.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 try {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     audioContext = new AudioContext();
@@ -14,36 +13,18 @@ try {
 } */
 var context = new AudioContext();
 var audioBuffer;
-=======
-var context = new AudioContext();
->>>>>>> parent of 65c8114... ???
 var sourceNode;
 var analyser;
 var javascriptNode;
 var ctx = $("#music").get()[0].getContext("2d");
 var gradient = ctx.createLinearGradient(0, 0, 0, 255);
 
-<<<<<<< HEAD
 gradient.addColorStop(1, '#FFFFFF');
 gradient.addColorStop(0, '#000000');
 
 setupAudioNodes();
 
 function setupAudioNodes() {
-=======
-gradient.addColorStop(1, '#A3FFFF');
-gradient.addColorStop(0, '#C5D5ED');
-
-if (!window.AudioContext) {
-    if (!window.webkitAudioContext) {
-        alert('AudioContext not found!');
-    }
-    window.AudioContext = window.webkitAudioContext;
-}
-
-function setupAudioNodes() {
-    "use strict";
->>>>>>> parent of 65c8114... ???
     javascriptNode = context.createScriptProcessor(2048, 1, 1);
     javascriptNode.connect(context.destination);
     analyser = context.createAnalyser();
@@ -55,7 +36,6 @@ function setupAudioNodes() {
     sourceNode.connect(context.destination);
 }
 
-<<<<<<< HEAD
 function loadSound(url) {
     var request = new XMLHttpRequest();
     request.open('GET', url, true);
@@ -78,55 +58,11 @@ function onError(e) {
 }
 
 javascriptNode.onaudioprocess = function() {
-=======
-setupAudioNodes();
-
-function playSound(buffer) {
-    "use strict";
-    sourceNode.buffer = buffer;
-    sourceNode.start(0);
-    window.setInterval(function (e) {
-    	console.log(sourceNode.context.currentTime);
-    }, 1000);
-}
-
-function onError(e) {
-    "use strict";
-    console.log(e);
-}
-
-function loadSound(url) {
-    "use strict";
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-        context.decodeAudioData(request.response, function (buffer) {
-            playSound(buffer);
-        }, onError);
-    };
-    request.send();
-}
-
-function drawSpectrum(array) {
-    "use strict";
-    var i = 0, value;
-    while (i < (array.length)) {
-        i = i + 1;
-        value = array[i];
-        ctx.fillRect(i * 8, 260 - value, 4, 260);
-    }
-}
-
-javascriptNode.onaudioprocess = function () {
-    "use strict";
->>>>>>> parent of 65c8114... ???
     var array = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(array);
     ctx.clearRect(0, 0, 2048, 255);
     ctx.fillStyle = gradient;
     drawSpectrum(array);
-<<<<<<< HEAD
 }
 
 function drawSpectrum(array) {
@@ -135,6 +71,3 @@ function drawSpectrum(array) {
         ctx.fillRect(i * 8, 260 - value, 4, 260);
     }
 }
-=======
-};
->>>>>>> parent of 65c8114... ???
